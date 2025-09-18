195a4d529db19a3f3ab0592a6a059ce0ecf011b9
Enderson Rufino Amorim <endersonrufino@gmail.com>
2025-09-18 12:10:08 -0300

    feat: add booking setion and removed p-6 of the card component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
 
 
 export default function Home() {
@@ -24,6 +27,28 @@ export default function Home() {
 
           <Image alt="banner" src="banner.svg" fill className="object-cover rounded-2xl" />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-[6] w-[6]">
+                  <AvatarImage src="https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg" />
+                </Avatar>
+                <p className="text-sm">Barbearia do ZÉ</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+              <p className="text-sm">Setembro</p>
+              <p className="text-2xl">18</p>
+              <p className="text-sm">14:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div >
   );
